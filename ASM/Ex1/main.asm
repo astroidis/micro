be46c952bc518748dc107d6a86011814d6a85339
astroidis <69013280+astroidis@users.noreply.github.com>
2021-01-18 19:41:15 +0300

    asm task 1 2

---
+++ ASM/Ex1/main.asm
@@ -0,0 +1,82 @@
+m:
+	;call	pp1
+	call	pp2
+	call	pp3
+	jmp		m
+
+pp1:
+	ldi		r16,	10		; load dec constant
+	ldi		r17,	0x10	; load hex constant 
+	ldi		r18,	0x20
+	ldi		r19,	0x30
+	mov		r1,		r16
+	mov		r2,		r17
+	mov		r3,		r18
+	mov		r4,		r19
+	ldi		xh,		0x01	; higher part of x address
+	ldi		xl,		0x60	; lower part of x address
+	st		x+,		r1		; load data from registers to ram
+	st		x+,		r2
+	st		x+,		r3
+	st		x+,		r4
+	ld		r10,	-x		; reading data at 0x0163
+	sts		0x0070,	r16
+	lds		r5,		0x0060
+	push	r1
+	push	r2
+	push	r3
+	push	r4
+	pop		r14
+	pop		r13
+	pop		r12
+	pop		r11
+	ret
+	
+pp2:
+	; A = N * 7 (N = 5)
+	.EQU	N = 5
+	ldi		r16,	N	; load N
+	ldi		r17,	7
+	mul		r16,	r17	; 5 * 7
+	
+	ldi		xh,		0x01
+	ldi		xl,		0x60
+	st		x+,		r0	; move result to 0x0160 and inc x
+
+	; B = (N * 3) mod 21
+	ldi		r16,	N
+	ldi		r17,	3
+	mul		r16,	r17
+	; modulo ...
+	st		x+,		r0
+
+	; C = 35
+	ldi		r16,	35
+	st		x+, 	r16
+
+	; D = 71 mod N
+	ldi		r16,	71 % N
+	st		x,		r16
+
+	ret
+
+pp3:
+	ldi		xh,		0x01
+	ldi		xl,		0x60
+
+	ld		r1,		x+
+	ld		r2,		x+
+	ld		r3,		x+
+	ld		r4,		x
+
+	push	r4
+	push	r3
+	push	r2
+	push	r1
+
+	pop		r11
+	pop		r12
+	pop		r13
+	pop		r14
+	
+	ret
